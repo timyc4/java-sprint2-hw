b8e5d4fd79c168a95bd0640777c1a4b820d233d7
timyc4 <t.dubitskiy@yandex.ru>
2023-08-07 15:12:16 +0300

    Create class

---
+++ src/MonthlyReport.java
@@ -0,0 +1,16 @@
+public class MonthlyReport {
+    String item_name;   // название товара
+    boolean is_expense; // одно из двух значений: TRUE или FALSE. Обозначает, является ли запись тратой (TRUE) или доходом (FALSE);
+    int quantity;       // количество закупленного или проданного товара;
+    double unit_price;  // стоимость одной единицы товара. Целое число.
+
+    public MonthlyReport(String item_name, boolean is_expense, int quantity, double unit_price) {
+        this.item_name = item_name;
+        this.is_expense = is_expense;
+        this.quantity = quantity;
+        this.unit_price = unit_price;
+    }
+
+
+
+}
